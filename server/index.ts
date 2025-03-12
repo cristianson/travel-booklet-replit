e73bc2f85d5cedcbfc0b60b162c5519f3ec334b3
Cristian <cristian.iordan2@gmail.com>
2025-03-12 13:17:55 +0200

    port 3000

--- server/index.ts
+++ server/index.ts
@@ -57,9 +57,9 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
+  // ALWAYS serve the app on port 3000
   // this serves both the API and the client
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 3000;
   server.listen(
     {
       port,
